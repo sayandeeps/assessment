fa9ccb5c51fe93396420cea322bd4f268c1c966b
Sayandeep <sayandeep11@gmail.com>
2024-02-08 00:38:22 +0530

    Global search workign

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import SearchPage from '../component/searchpage'
+// SavedPhotosPage.js
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectSavedPhotos } from "../store/photoSlice";
 
-const page = () => {
+const SavedPhotosPage = () => {
+  const savedPhotos = useSelector(selectSavedPhotos);
+
   return (
     <div>
-
-<SearchPage />
-
+      <h1>Saved Photos</h1>
+      <ul>
+        {savedPhotos.map((photo) => (
+          <li key={photo.id}>
+            <img src={photo.url} alt={photo.title} />
+            <p>{photo.title}</p>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default SavedPhotosPage;
